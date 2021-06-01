b358cc822d30f7985eaba0897987a319ba281445
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-06-01 09:55:41 -0400

    added file structure and server file

---
+++ Develop/server.js
@@ -0,0 +1,17 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(require("./controllers"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", { useNewUrlParser: true });
+
+app.listen(PORT, () => {
+    console.log(`Now listening on PORT ${PORT}!`)
+});
